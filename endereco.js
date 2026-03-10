3e630005e02d827895d49491f71fadfb23c1f350
iagohenriquelima-web <iago.henrique.lima@escola.pr.gov.br>
2026-03-10 14:39:11 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,9 @@
+console.log("Endereço completo");
+
+let rua = "rua das flores";
+let numero = 489;
+let cidade ="curitiba"; 
+
+let endereco = rua + "," + numero + "-" + cidade;   
+//console.log(endereco); 
+console.log("o endereco correto é: " + endereco);

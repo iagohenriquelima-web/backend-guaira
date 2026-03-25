6e4dde03de0365b71e40ca7835e1854ef9be6903
iagohenriquelima-web <iago.henrique.lima@escola.pr.gov.br>
2026-03-25 18:58:02 +0000

    versao3

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,7 @@ let tempCelsius = 18.5;
 
 const tempFahrenheit = tempCelsius * (9/5) + 32;
 const tempKenvin = tempCelsius + 273.15;
+
+console.log(`A temperatura de ${cidade} está ${tempCelsius} cº`); 
+console.log(`Fahrenheit: ${tempFahrenheit.toFixed(1)} Fº);
+console.log(`Kelvin: ${tempKenvin.toFixed(1)} Kº`);

7a9ef91f2052c979c47854f7585ec6ef258ed5f2
iagohenriquelima-web <iago.henrique.lima@escola.pr.gov.br>
2026-03-25 18:30:07 +0000

    versaol

---
+++ produto.js
@@ -0,0 +1,9 @@
+const produto = "mochila impermeável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessório";
+
+console.log(`o produto é: $(produto).` );
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`O estoque ainda possui ${estoque} mochilas`);
+console.log(`A categoria de ${produto} é ${categoria}.`);

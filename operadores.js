52548151c740d812393a5c5125aac3b0b2c06bb1
iagohenriquelima-web <iago.henrique.lima@escola.pr.gov.br>
2026-03-31 17:36:54 +0000

    versaol

---
+++ operadores.js
@@ -0,0 +1,28 @@
+//OPERADOR E &&
+let maioriadade = true;
+let possuiDocumento = false;
+
+if (maioriadade && possuiDocumento) {
+    console.log('pode entrar na festa!')
+} else {
+    console.log('não pode entra na festa.')
+};
+
+// operador ou ||
+let cartaoCretido = false;
+let dinheiro = false;
+
+if (cartaoCretido || dinheiro){
+    console.log('pode fazer a compra.')
+} else {
+    console.log('não pode fazer a compra.')
+};
+
+// operador not !
+let estachovendo = true;
+
+if (!estachovendo) {
+    console.log('pode sair sem guarda-chuva.')
+} else {
+    console.log('leve o guarda-chuva.')
+};

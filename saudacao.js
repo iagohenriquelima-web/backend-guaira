71191f19b89b517e8f6a8977e438d7118f59d264
iagohenriquelima-web <iago.henrique.lima@escola.pr.gov.br>
2026-05-04 19:19:39 +0000

    versaol

---
+++ saudacao.js
@@ -0,0 +1,8 @@
+let filaEspera = ["ana", "bruno", "carlos"];
+
+console.log(filaEspera);
+filaEspera.push("diego"); //adiciona um elemento no final da lista
+console.log(filaEspera);
+
+filaEspera.shift();// remove o priemiro elelmento da lista
+console.log(filaEspera);

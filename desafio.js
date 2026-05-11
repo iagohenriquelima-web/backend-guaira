09847501dd56c5bf7db3f59916242edd0a83df8c
iagohenriquelima-web <iago.henrique.lima@escola.pr.gov.br>
2026-05-11 19:10:52 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+console.log("perfil do jogador")
+
+let nomejogador= "neymar";
+let posicao = "atacante";
+let idadejogador = "34";
+let time = "barcelona";
+let gols = "450";
+
+console.log(`O jogador ${nomejogador},é ${posicao},tem ${idadejogador} anos ,e joga no ${time}, tambem fez ${gols} gols `);
